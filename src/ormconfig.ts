40f9d8d8d4d28bb1f1c72a2637089bf348e5a023
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2023-02-15 21:14:50 -0300

    Config docker-compose and env

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,18 +1,18 @@
 /* eslint-disable prettier/prettier */
-import * as path from 'path';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { UserEntity } from './database/entities/user.entity';
 
 dotenv.config();
 
 export const config: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.DATABASE_PORT || 5432),
+  host: 'localhost',
+  port: 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [path.resolve(__dirname, './database/entities/*.entity.{js,ts}')],
+  entities: [UserEntity],
   synchronize: true,
   logging: false,
 };
